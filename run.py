3c1f8e5f73d9cf2d2ae932a5dc0abecf8b18b499
sriki18 <srikiranc@gmail.com>
2019-07-22 11:03:06 -0700

    ADD: `cdbrute` to `run.py`

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 import time
 from staph.utils.dev import compute_devs_min as cdmin
+from staph.utils.dev import compute_devs_brute as cdbrute
 
 
 if __name__ == "__main__":
@@ -29,4 +30,27 @@ if __name__ == "__main__":
             niter=niter,
             problem_type=1,
             n_procs=ncores,
+        )
+    elif choice == 2:
+        nrep = 1000
+        # For wide bounds
+        fname = "results/6021324_DEMC_40000g_16p6mod1ds0se_staph1o6.mat"
+        ncores = np.int32(sys.argv[2])
+        ind = np.int32(sys.argv[3])
+        assert ind >= 1
+        desol_ind = np.arange(ind - 1, ind)
+        nstep = 200000
+        npts = 6
+        cdbrute(
+            filename=fname,
+            npts=npts,
+            nrep=nrep,
+            seed=seed,
+            desol_ind=desol_ind,
+            nstep=nstep,
+            problem_type=1,
+            n_procs=ncores,
+            lims={"d1l": 1.5, "d1u": 3.5, "b2l": 5, "b2u": 30},
+            nb2=5,
+            nd1=5,
         )
