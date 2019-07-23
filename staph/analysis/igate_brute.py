dc9ae6e380c52be068cda5dbfba7aa03b8e6bc62
srikirac <srikirac@uci.edu>
2019-07-23 15:26:33 -0700

    ADD: `t_type` to `igate_brute`

--- staph/analysis/igate_brute.py
+++ staph/analysis/igate_brute.py
@@ -29,7 +29,7 @@ def igate(filename: str):
         d1listu = data["d1listu"]
         nb2 = data["nb2"]
         nd1 = data["nd1"]
-
+        t_type = data["t_type"].tolist()
     xx = np.zeros([len(b2listu), len(d1listu)])
     yy = np.zeros([len(b2listu), len(d1listu)])
     zz = np.zeros([len(b2listu), len(d1listu)])
@@ -43,7 +43,7 @@ def igate(filename: str):
     for ind1 in range(nb2):
         for ind2 in range(nd1):
             linear_ind = ind1 * nd1 + ind2
-            b2, d1 = transform_x([b2listu[ind1], d1listu[ind2]])
+            b2, d1 = transform_x([b2listu[ind1], d1listu[ind2]], t_type=t_type)
             xx[ind1, ind2] = b2
             yy[ind1, ind2] = d1
             zz[ind1, ind2] = tot_devs[linear_ind]
@@ -61,5 +61,5 @@ def igate(filename: str):
                 plt.title(title)
     plt.xlabel("b2")
     plt.ylabel("d1")
-    ax.plot_wireframe(xx, yy, zz)
+    # ax.plot_wireframe(xx, yy, zz)
     plt.show()
