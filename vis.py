f9a30c34b908cbd8784c5fd96e58eee7bc902744
srikirac <srikirac@uci.edu>
2019-07-22 21:57:27 -0700

    CHG: Update file names in opt1=2, `vis.py`

--- vis.py
+++ vis.py
@@ -35,5 +35,17 @@ if option1 == 1:
     ]
     b2d1_igate(filenames=fnames, choice=option2)
 elif option1 == 2:
-    filename = "results/6021324_10rep0se_0to0b2d1_1o4_cpu.npz"
+    filename = "results/6021324_1000rep0se_0to0b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_1to1b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_2to2b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_3to3b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_4to4b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_5to5b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_6to6b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_7to7b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_8to8b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_9to9b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_10to10b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_11to11b2d1_1o4_cpu.npz"
+    # filename = "results/6021324_1000rep0se_12to12b2d1_1o4_cpu.npz"
     bru_igate(filename=filename)
