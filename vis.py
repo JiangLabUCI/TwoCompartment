ac97757d7d5fb69fcc133de8dcaa755b8cbc345e
srikirac <srikirac@uci.edu>
2019-07-24 16:31:51 -0700

    ADD: `igate_ntest` to `vis.py`

--- vis.py
+++ vis.py
@@ -1,6 +1,7 @@
 import sys
 from staph.analysis.landscape import igate as b2d1_igate
 from staph.analysis.igate_brute import igate as bru_igate
+from staph.analysis.igate_ntest import igate as igate_ntest
 
 option1 = int(sys.argv[1])
 
@@ -28,3 +29,9 @@ elif option1 == 2:
             + "b2d1_1o4_cpu.npz"
         )
     bru_igate(filenames=fnames)
+elif option1 == 3:
+    fnames = []
+    flist = [9]
+    for ind in flist:
+        fnames.append("results/ntest.o7721941." + str(ind))
+    igate_ntest(filenames=fnames)
