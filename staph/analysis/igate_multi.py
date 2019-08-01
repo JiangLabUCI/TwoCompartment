8b1e7d15ea2600bd1ef7a98a59f76a98ce6940d8
srikirac <srikirac@uci.edu>
2019-08-01 13:40:37 -0700

    CHG: don't plot overflow but provide info
    - Earlier all trajectories were being plotted
    - Now ones exceeding 1e8 or overflowing are not plotted
    - Their status is printed instead

--- staph/analysis/igate_multi.py
+++ staph/analysis/igate_multi.py
@@ -15,6 +15,7 @@ def igate(fname="results/pred_10rep200nstr1hypF_multi.npz", option1=2):
         explosion = data["explosion"]
         extinction = data["extinction"]
         pop_flag = data["pop_flag"]
+        status = data["status"]
     print(f"p_response = {np.mean(explosion)}")
     print(f"p_car = {1 - np.mean(explosion + extinction)}")
     print(f"p_unaffected = {np.mean(extinction)}")
@@ -26,6 +27,9 @@ def igate(fname="results/pred_10rep200nstr1hypF_multi.npz", option1=2):
                 t = data["t"]
             for ind in range(len(popH)):
                 y = popH[ind] + popI[ind]
+                if (np.max(y) > 1e8) or (np.min(y) < 0):
+                    print(ind, status[ind], np.max(y), np.min(y))
+                    continue
                 if extinction[ind]:
                     plt.plot(t[ind], y, color="green", alpha=0.3)
                 elif explosion[ind]:
