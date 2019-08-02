c60b081e1622e3abca405b4424688c4c478495f4
srikirac <srikirac@uci.edu>
2019-08-02 15:31:01 -0700

    CHG: Use `pop_time` in `igate_multi`

--- staph/analysis/igate_multi.py
+++ staph/analysis/igate_multi.py
@@ -1,16 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from .f2 import partition_plot
+from .f3 import pop_time
 
 
 def igate(fname="results/pred_10rep200nstr1hypF_multi.npz", option1=2):
-    part_cols = ["#4daf4a", "#ff7f00", "#e41a1c"]
     with np.load(fname, allow_pickle=True) as data:
         pres = data["pres"]
         ps = data["ps"]
         pcar = data["pcar"]
-        nstep = data["nstep"]
-        nrep = data["nrep"]
         tref = data["tref"]
         explosion = data["explosion"]
         extinction = data["extinction"]
@@ -20,23 +18,23 @@ def igate(fname="results/pred_10rep200nstr1hypF_multi.npz", option1=2):
     print(f"p_car = {1 - np.mean(explosion + extinction)}")
     print(f"p_unaffected = {np.mean(extinction)}")
     if option1 == 1:
-        if pop_flag:
-            with np.load(fname, allow_pickle=True) as data:
-                popH = data["popH"]
-                popI = data["popI"]
-                t = data["t"]
-            for ind in range(len(popH)):
-                y = popH[ind] + popI[ind]
-                if (np.max(y) > 1e8) or (np.min(y) < 0):
-                    print(ind, status[ind], np.max(y), np.min(y))
-                    continue
-                if extinction[ind]:
-                    plt.plot(t[ind], y, color="green", alpha=0.3)
-                elif explosion[ind]:
-                    plt.plot(t[ind], y, color="darkred", alpha=0.3)
-                else:
-                    plt.plot(t[ind], y, color="darkblue", alpha=0.3)
-            plt.show()
+        with np.load(fname, allow_pickle=True) as data:
+            popH = data["popH"]
+            popI = data["popI"]
+            t = data["t"]
+            new_exp = data["new_exp"]
+            new_ext = data["new_ext"]
+        for ind in range(len(t)):
+            y = popH[ind] + popI[ind]
+            if (np.max(y) > 1e8) or (np.min(y) < 0):
+                print(f"Index = {ind}, max = {np.max(y)}, min = {np.min(y)}")
+                if pop_flag:
+                    print(f"Status : {status[ind]}")
+                continue
+        pop_time(
+            t=t, popH=popH, popI=popI, extinction=new_ext, explosion=new_exp, log=True
+        )
+        plt.show()
     elif option1 == 2:
         # ax = plt.subplots()
         ax = plt.subplot()
