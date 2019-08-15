5b433f923340156de90a5faa121775ce6ece557e
sriki18 <srikiranc@gmail.com>
2019-08-15 16:43:46 -0700

    ADD: test to `test_thresh`

--- tests/test_thresh.py
+++ tests/test_thresh.py
@@ -11,17 +11,28 @@ def test_gb_thresh():
     p = np.array(norig) / ntot
     npts = 6
     nrep = 20
-    low = 899
-    high = 1005
     fake_loads = np.zeros([npts, nrep])
+
     for ind1 in range(npts):
         switch_index = np.int(p[ind1] * nrep)
         print(f"switch index {switch_index}")
         fake_loads[ind1, :switch_index] = thresh + 1
         fake_loads[ind1, switch_index:] = thresh - 1
     # print(fake_loads)
-    best_thresh, best_dev, devs, _ = get_best_thresh(fake_loads, low=low, high=high)
-    assert devs.shape[0] == high - low
+    best_thresh, best_dev, devs, _ = get_best_thresh(fake_loads)
+    assert devs.shape[0] == 2
     assert best_thresh == thresh
     assert np.abs(best_dev) < 1e-12
 
+    np.random.seed(0)
+    for ind1 in range(npts):
+        switch_index = np.int(p[ind1] * nrep)
+        n1 = len(np.arange(switch_index))
+        n2 = nrep - n1
+        fake_loads[ind1, :switch_index] = thresh + 1 * np.random.randint(2, 10, n1)
+        fake_loads[ind1, switch_index:] = thresh - 1 * np.random.randint(2, 10, n2)
+    # print(fake_loads)
+    best_thresh, best_dev, devs, _ = get_best_thresh(fake_loads)
+    assert devs.shape[0] == len(np.unique(fake_loads))
+    assert best_thresh == 999
+    assert np.abs(best_dev) < 1e-12
