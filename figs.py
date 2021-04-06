4a425d27dee5b83c94eea55b9f0b8642fbbf2cc5
sriki18 <srikiranc@gmail.com>
2021-04-05 22:44:31 -0400

    ADD: plot_posterior_dose_resp in figs.py

--- figs.py
+++ figs.py
@@ -1,9 +1,10 @@
 from staph.analysis.f2 import f2
 from staph.analysis.f3 import f3
 from staph.analysis.phase_plot import make_phase_plot
-from staph.analysis.plot_posterior import plot_parameter_posteriors
+from staph.analysis.plot_posterior import plot_parameter_posteriors, plot_posterior_dose_resp
 
 # f2()
 # f3(display=True)
 # make_phase_plot(disp=False, save=True)
 plot_parameter_posteriors()
+# plot_posterior_dose_resp()
